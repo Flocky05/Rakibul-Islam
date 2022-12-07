033d2aa738c038ca40993bf2ceda8da19dd73696
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-12-07 17:23:49 +0600

    added navbar

---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <h2 className="text-3xl fornt-bold">Rakibul Islam</h2>
+        </div>
+        <div className="flex-none">
+          <ul className=" menu-horizontal p-3">
+            <li>
+              <a href="/" className="p-3">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/" className="p-3">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/" className="p-3">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="/" className="p-3">
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href="/" className="p-3">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
